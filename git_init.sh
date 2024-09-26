a24da8e51d59e0a066a1946d25af0558f57678f4
xxx <xxx>
2024-09-26 20:01:20 +0800

    2024-09-26 20:01:20

---
+++ git_init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+user_name="dddw1216"
+user_email="dddw1216@gmail.com"
+
+git config --local user.name ${user_name}
+git config --local user.email ${user_email}
---
+++ project_init/git/git_init.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+user_name="xxx"
+user_email="xxx"
+
+git config user.name ${user_name}
+git config user.email ${user_email}
