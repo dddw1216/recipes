445f2afc812f13f1b7a7f4279bdfffaa34cf9205
dddw1216 <dddw1216@gmail.com>
2024-09-26 20:01:50 +0800

    2024-09-26 20:01:50

--- git_push.sh
+++ git_push.sh
@@ -4,8 +4,8 @@ set -x
 find . -name "*.out"  | xargs rm -f
 find . -name "core.*"  | xargs rm -f
 
-user_name="xxx"
-user_email="xxx"
+user_name="dddw1216"
+user_email="dddw1216@gmail.com"
 
 git config user.name ${user_name}
 git config user.email ${user_email}
