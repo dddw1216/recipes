832718b22f9e8ce8299d735be5b9c35804580c64
dddw1216 <dddw1216@gmail.com>
2024-09-27 14:22:13 +0800

    2024-09-27 14:22:13

--- project_init/git/git_init.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-user_name="xxx"
-user_email="xxx"
-
-git config user.name ${user_name}
-git config user.email ${user_email}
--- project_init/git/git_pull.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-set -x
-
-git fetch origin
-git merge origin/main
