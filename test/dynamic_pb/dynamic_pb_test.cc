9b8ea9a87d94bfd8eda8cdba118b4d980dcdd86b
dddw1216 <dddw1216@gmail.com>
2024-09-27 16:32:22 +0800

    2024-09-27 16:32:22

--- test/dynamic_pb/dynamic_pb_test.cc
+++ test/dynamic_pb/dynamic_pb_test.cc
@@ -3,6 +3,10 @@
 #include <iostream>
 #include <string>
 
-int main() {
+#include "dynamic_pb/proto/item_node.pb.h"
+
+int main(int argc, char** argv) {
+  recipes::ItemNode item_node;
+  std::cout << "item_node:\n" << item_node.DebugString() << std::endl;
   return 0;
 }
