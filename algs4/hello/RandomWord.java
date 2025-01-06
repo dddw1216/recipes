155bbb4056820a0b1c1f9f9903b4252c71ea5fce
dddw1216 <dddw1216@gmail.com>
2025-01-06 23:13:23 +0800

    2025-01-06 23:13:23

---
+++ algs4/hello/RandomWord.java
@@ -0,0 +1,12 @@
+/* *****************************************************************************
+ *  Name:              Ada Lovelace
+ *  Coursera User ID:  123456
+ *  Last modified:     October 16, 1842
+ **************************************************************************** */
+
+public class RandomWord {
+    // prints "Hello, World"
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+    }
+}
